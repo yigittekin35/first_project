652d32a3ff88659a2b174778a2744b798f30c7a4
Yigit Atahan Tekin <yigitatahantekin@gmail.com>
2022-07-11 16:34:07 +0300

    Models definition

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# python manage.py migrate
+# python manage.py makemigrations first_app
+# python manage.py migrate
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length = 264, unique = True)
+
+    def __str__(self):
+        return self.top_name
+
+class WebPage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete = models.PROTECT)
+    name = models.CharField(max_length = 264, unique = True)
+    url = models.URLField(unique = True)
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(WebPage, on_delete = models.PROTECT)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
--- first_project/settings.py
+++ first_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, ],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
